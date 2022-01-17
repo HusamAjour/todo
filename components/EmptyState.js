d93c6d2842eb263c72977c92fb09d8a4d8f87392
Husam Ajour <husamaj278@gmail.com>
2022-01-17 22:24:36 +0200

    add login methods with facebook github and twitter

---
+++ components/EmptyState.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+import { useAuth } from "@/lib/auth";
+
+import {
+  GoogleButton,
+  TwitterButton,
+  FacebookButton,
+  GithubButton,
+} from "./LoginButtons";
+
+function EmptyState(props) {
+  const auth = useAuth();
+
+  return (
+    <>
+      <h1 className="m-0 text-white text-center text-30px mobile:text-[20px] tracking-[18px] font-bold">
+        TODO
+      </h1>
+      <p className="text-center text-white text-xl">
+        Login with your favorite platform
+      </p>
+      <div className="flex items-center justify-center flex-col w-[280px] sm:w-[400px] m-auto mt-10">
+        <GoogleButton onClick={() => auth.signinWithGoogle()} />
+        <FacebookButton onClick={() => auth.singinWithFacebook()} />
+        <TwitterButton onClick={() => auth.singinWithTwitter()} />
+        <GithubButton onClick={() => auth.singinWithGithub()} />
+      </div>
+    </>
+  );
+}
+
+export default EmptyState;
--- components/Header.js
+++ components/Header.js
@@ -46,70 +46,54 @@ function Header(props) {
             id="modeIcon"
             onClick={toggleMode}
           />
-          {auth.user ? (
-            <Menu as="div" className="ml-3 relative z-20">
-              <div>
-                <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none">
-                  <span className="sr-only">Open user menu</span>
-                  <Image
-                    src={auth.user.photoUrl}
-                    alt="avatar"
-                    width={30}
-                    height={30}
-                    className="rounded-full drop-shadow-2xl"
-                  />
-                </Menu.Button>
-              </div>
-              <Transition
-                as={Fragment}
-                enter="transition ease-out duration-100"
-                enterFrom="transform opacity-0 scale-95"
-                enterTo="transform opacity-100 scale-100"
-                leave="transition ease-in duration-75"
-                leaveFrom="transform opacity-100 scale-100"
-                leaveTo="transform opacity-0 scale-95"
-              >
-                <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-                  <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        href="#"
-                        className={
-                          "block px-4 py-2 text-sm text-gray-700 font-bold border-b"
-                        }
-                      >
-                        {auth.user.name}
-                      </a>
-                    )}
-                  </Menu.Item>
-                  <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        href="#"
-                        className={"block px-4 py-2 text-sm text-gray-700"}
-                      >
-                        <button onClick={() => auth.signout()}>Logout</button>
-                      </a>
-                    )}
-                  </Menu.Item>
-                  {/* <Menu.Item>
-                    {({ active }) => (
-                      <a
-                        href="#"
-                        className={"block px-4 py-2 text-sm text-gray-700"}
-                      >
-                        Settings
-                      </a>
-                    )}
-                  </Menu.Item> */}
-                </Menu.Items>
-              </Transition>
-            </Menu>
-          ) : (
-            <button onClick={() => auth.signinWithGoogle()}>
-              Sign in with Google
-            </button>
-          )}
+          <Menu as="div" className="ml-3 relative z-20">
+            <div>
+              <Menu.Button className="bg-gray-800 flex text-sm rounded-full focus:outline-none">
+                <span className="sr-only">Open user menu</span>
+                <Image
+                  src={auth.user.photoUrl}
+                  alt="avatar"
+                  width={30}
+                  height={30}
+                  className="rounded-full drop-shadow-2xl"
+                />
+              </Menu.Button>
+            </div>
+            <Transition
+              as={Fragment}
+              enter="transition ease-out duration-100"
+              enterFrom="transform opacity-0 scale-95"
+              enterTo="transform opacity-100 scale-100"
+              leave="transition ease-in duration-75"
+              leaveFrom="transform opacity-100 scale-100"
+              leaveTo="transform opacity-0 scale-95"
+            >
+              <Menu.Items className="origin-top-right absolute right-0 mt-2 w-48 rounded-md shadow-lg py-1 bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={
+                        "block px-4 py-2 text-sm text-gray-700 font-bold border-b"
+                      }
+                    >
+                      {auth.user.name}
+                    </a>
+                  )}
+                </Menu.Item>
+                <Menu.Item>
+                  {({ active }) => (
+                    <a
+                      href="#"
+                      className={"block px-4 py-2 text-sm text-gray-700"}
+                    >
+                      <button onClick={() => auth.signout()}>Logout</button>
+                    </a>
+                  )}
+                </Menu.Item>
+              </Menu.Items>
+            </Transition>
+          </Menu>
         </div>
       </div>
       <div className="mb-25px add-item-section">
@@ -125,7 +109,6 @@ function Header(props) {
             />
           </div>
         </form>
-        {/* <button onClick={focusInput}>Click</button> */}
       </div>
     </>
   );
